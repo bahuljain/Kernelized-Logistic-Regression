fa90974f64ed328e8ba94cf3caf18cdff3aee387
bahuljain <bkj2111@columbia.edu>
2015-11-05 00:56:28 -0500

    batch gradient descent full and final completed

--- BatchGradientDescent.m
+++ BatchGradientDescent.m
@@ -1,6 +1,5 @@
 function [W, iterations, costT] = BatchGradientDescent(Y,K,YT,KT,eta)
-%BatchGradientDescent This function finds the optimum model by
-%incrementally updating the model.
+%% BatchGradientDescent This function finds the optimum model by incrementally updating the model.
 
 % Y - Vector of desired output values in training set 
 % K - gram matrix for training input values
@@ -12,16 +11,23 @@ function [W, iterations, costT] = BatchGradientDescent(Y,K,YT,KT,eta)
 % iterations - number of iterations in which BGD converges
 
 n = length(Y);
+
+%% Assigning all ones to the model in the beginning
 W = ones(n,1);
 
+%% Setting parameter values (eta given by user)
 epsilon = 1e-2;
 lambda = 1e-3;
+
+%% First update
 delta = (K*Y - K*(Y .* Sigma(Y .* (W'*K)')))/(-n)+ 2*lambda*W;
 iterations = 0;
+
+
+%% Iteratively updating the model using stochastic gradient descent
 tic
-while norm(delta) > epsilon && toc<10% && iterations < 50000
+while norm(delta) > epsilon && toc<10
     iterations = iterations + 1;
-    %costt(iterations) = CostFunction(W, K, Y);
     costT(iterations) = CostFunction(W, KT, YT);
     delta = (K*Y - K*(Y .* Sigma(Y .* (W'*K)')))/(-n)+ 2*lambda*W;
     gradient = norm(delta)
