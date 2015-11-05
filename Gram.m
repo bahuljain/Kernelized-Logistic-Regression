71d6dd40b7d54e78afe77f9c9245d55ccdeed19b
bahuljain <bkj2111@columbia.edu>
2015-11-05 01:40:19 -0500

    FULL AND FINAL DONE

--- Gram.m
+++ Gram.m
@@ -1,6 +1,10 @@
 function [K] = Gram(X1, X2, kappa)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
+%% Gram Matrix
+% X1 - Matrix of input training/testing points
+% X2 - Matrix of input TRAINING points only 
+% kappa - kappa value
+% K - gram matrix
+
 n = length(X1);
 K = zeros(n);
 for i = 1:n
@@ -8,7 +12,5 @@ for i = 1:n
         K(i,j) = exp(-1*norm(X1(i,:) - X2(j,:))^2 / kappa);
     end
 end
-
-
 end
 
--- Kappa.m
+++ Kappa.m
@@ -1,17 +1,16 @@
 function K = Kappa(X)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+%% Kappa
+% X - matrix of input vectors
+% K - Kappa value
+
 n = length(X);
 K = 0;
-
 for i = 1:n
     for j = 1:n
         K = K + norm(X(i,:)-X(j,:))^2;
     end
 end
-
 K = K/(n^2);
-
 
 end
 
